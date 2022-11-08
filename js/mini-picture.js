5450f6f2c53733b83ecfd688d4defe544a09e6e8
Kseniya Sakurova <kseniasakurova@gmail.com>
2022-11-09 02:50:39 +0300

    разбивает код на функции

--- js/mini-picture.js
+++ js/mini-picture.js
@@ -2,15 +2,30 @@ import { generatePhotos } from './data.js';
 const usersPicturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const usersPictures = generatePhotos();
-const picturesListFragment = document.createDocumentFragment();
-usersPictures.forEach(({url, likes, comments}) => {
+
+function getUserPicture({url, likes, comments}) {
   const userPicture = pictureTemplate.cloneNode(true);
   userPicture.querySelector('.picture__img').src = url;
   userPicture.querySelector('.picture__likes').textContent = likes;
   userPicture.querySelector('.picture__comments').textContent = comments.length;
-  picturesListFragment.appendChild(userPicture);
-});
-usersPicturesList.appendChild(picturesListFragment);
+  return userPicture;
+}
 
+function getPicturesListFragment(usersPicturesData) {
+  const picturesListFragment = document.createDocumentFragment();
 
+  usersPicturesData.forEach(({url, likes, comments}) => {
+    const userPictureFilled = getUserPicture({url, likes, comments});
+    picturesListFragment.appendChild(userPictureFilled);
+  });
+  return picturesListFragment;
+}
+
+function renderUsersPictures(usersPicturesData) {
+  const picturesListFragmentFilled = getPicturesListFragment(usersPicturesData);
+  usersPicturesList.appendChild(picturesListFragmentFilled);
+}
+
+const usersPicturesMockData = generatePhotos();
+
+renderUsersPictures(usersPicturesMockData);
