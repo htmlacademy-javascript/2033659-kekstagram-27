a73d1bbf297eafad27f449c99d424455e9867848
Kseniya Sakurova <kseniasakurova@gmail.com>
2022-11-11 21:36:11 +0300

    добавляет блок просмотра большого изображения

---
+++ js/big-picture.js
@@ -0,0 +1,78 @@
+function createComment ({avatar, message, name}) {
+  const commentElement = document.createElement('li');
+  const avatarElement = document.createElement('img');
+  const textElement = document.createElement('p');
+
+  avatarElement.classList.add('social__picture');
+  avatarElement.src = avatar;
+  avatarElement.alt = name;
+  avatarElement.width = 35;
+  avatarElement.height = 35;
+
+  textElement.classList.add('social__text');
+  textElement.textContent = message;
+
+  commentElement.classList.add('social__comment');
+  commentElement.appendChild(avatarElement);
+  commentElement.appendChild(textElement);
+
+  return commentElement;
+}
+
+function renderComments (commentsList) {
+  const socialComments = document.querySelector('.social__comments');
+
+  socialComments.innerHTML = '';
+
+  commentsList.forEach((comment) => {
+    const filledCommentElement = createComment(comment);
+    socialComments.appendChild(filledCommentElement);
+  });
+}
+
+function fillBigPicture ({url, likes, comments, description}) {
+  const bigPictureOverlay = document.querySelector('.big-picture');
+  bigPictureOverlay.querySelector('.big-picture__img img').src = url;
+  bigPictureOverlay.querySelector('.likes-count').textContent = likes;
+  bigPictureOverlay.querySelector('.comments-count').textContent = comments.length;
+  bigPictureOverlay.querySelector('.social__caption').textContent = description;
+}
+
+function openBigPicture ({url, likes, comments, description}) {
+  const bigPictureOverlay = document.querySelector('.big-picture');
+  const bodyElement = document.querySelector('body');
+  const closeBigPictureButton = bigPictureOverlay.querySelector('.big-picture__cancel');
+  const commentCount = bigPictureOverlay.querySelector('.social__comment-count');
+  const commentsLoadButton = bigPictureOverlay.querySelector('.comments-loader');
+
+  commentCount.classList.add('hidden');
+  commentsLoadButton.classList.add('hidden');
+  fillBigPicture({url, likes, comments, description});
+  renderComments(comments);
+  bodyElement.classList.add('modal-open');
+  closeBigPictureButton.addEventListener('click', closeBigPicture);
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' || event.key === 'Esc' || event.keyCode === 27) {
+      closeBigPicture(event);
+    }
+  });
+  bigPictureOverlay.classList.remove('hidden');
+}
+
+function closeBigPicture (event) {
+  event.preventDefault();
+  const bigPictureOverlay = document.querySelector('.big-picture');
+  const bodyElement = document.querySelector('body');
+  const closeBigPictureButton = bigPictureOverlay.querySelector('.big-picture__cancel');
+  const commentCount = bigPictureOverlay.querySelector('.social__comment-count');
+  const commentsLoadButton = bigPictureOverlay.querySelector('.comments-loader');
+
+  commentCount.classList.remove('hidden');
+  commentsLoadButton.classList.remove('hidden');
+
+  bodyElement.classList.remove('modal-open');
+  bigPictureOverlay.classList.add('hidden');
+  closeBigPictureButton.removeEventListener('click', closeBigPicture);
+}
+
+export { openBigPicture };
--- js/mini-picture.js
+++ js/mini-picture.js
@@ -1,21 +1,29 @@
 import { generatePhotos } from './data.js';
+import { openBigPicture } from './big-picture.js';
+
 const usersPicturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 
-function getUserPicture({url, likes, comments}) {
+function getUserPicture({url, likes, comments, description}) {
   const userPicture = pictureTemplate.cloneNode(true);
+
   userPicture.querySelector('.picture__img').src = url;
   userPicture.querySelector('.picture__likes').textContent = likes;
   userPicture.querySelector('.picture__comments').textContent = comments.length;
+  userPicture.addEventListener('click', (event) => {
+    event.preventDefault();
+    openBigPicture({url, likes, comments, description});
+  });
+
   return userPicture;
 }
 
 function getPicturesListFragment(usersPicturesData) {
   const picturesListFragment = document.createDocumentFragment();
 
-  usersPicturesData.forEach(({url, likes, comments}) => {
-    const userPictureFilled = getUserPicture({url, likes, comments});
+  usersPicturesData.forEach(({url, likes, comments, description}) => {
+    const userPictureFilled = getUserPicture({url, likes, comments, description});
     picturesListFragment.appendChild(userPictureFilled);
   });
   return picturesListFragment;
